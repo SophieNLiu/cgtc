703d2af4179e8ef806516abcba48d85fae081553
Tammy Ding <tammyding@gmail.com>
2026-05-05 20:29:09 -0600

    added vision

--- src/components/homePageComponents/hero.js
+++ src/components/homePageComponents/hero.js
@@ -37,19 +37,34 @@ const Hero = () => {
       </Typography>
 
       {/* DESCRIPTION */}
-      <Typography
-        textAlign="left"
-        sx={{
-          maxWidth: { xs: '100%', md: 800 },
-          whiteSpace: 'pre-line',
-        }}
-      >
-        {`卡尔加里恩理华人基督教会诚挚邀请各界朋友参加每周日下午举行的主日崇拜。
+<Typography
+  textAlign="left"
+  sx={{
+    maxWidth: { xs: '100%', md: 800 },
+    whiteSpace: 'pre-line',
+  }}
+>
+{`卡尔加里恩理华人基督教会诚挚邀请各界朋友参加每周日下午举行的主日崇拜。
 我们是一间以基督为中心、以圣经为根基的教会，致力于传扬耶稣基督的恩典与真理。
 
 本教会的会众来自不同背景，却同心追求在信仰上成长，在神的话语中扎根，在敬拜、祷告与团契中彼此建立。我们盼望每一位来到教会的朋友，都能经历神真实的同在，认识祂的爱，并在生命中得着更新与改变。
 
 无论您是长期信主的基督徒，刚开始寻求信仰的朋友，或是对基督信仰感到好奇，我们都热忱欢迎您的到来。
+
+`}
+
+<Box component="span" sx={{ fontWeight: 'bold' }}>
+我们的异象：往下扎根，向上结果，连于基督。
+</Box>
+
+{`
+
+扎根在真理中～读经，                   
+扎根在圣灵中～祷告，                   
+扎根在教会中～敬拜，团契，服侍
+结果子：圣灵的果子，福音的果子
+
+仁爱、喜乐、和平、忍耐、恩慈、良善、信实、温柔、节制。
 
 主日崇拜时间：每周日下午 2:00
 教会地址：14640 6 St SW, Calgary, AB T2Y 0E1
@@ -68,7 +83,7 @@ Location: 14640 6 St SW, Calgary, AB T2Y 0E1
 
 We look forward to worshiping with you！
 `}
-      </Typography>
+</Typography>
 
       {/* BUTTONS */}
       <Button component={Link} to="/contact">
